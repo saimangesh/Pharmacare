76e8f415debfded67e4364ce299d3bfcfb10c5ea
Sai Mangesh <47791273+saimangesh@users.noreply.github.com>
2024-03-02 02:10:22 +0530

    Update client.sh

--- jenkins/client.sh
+++ jenkins/client.sh
@@ -1,4 +1,19 @@
+#!/bin/bash
+set -e
+
+# kill the react client
+pid=$(sudo lsof -t -i:3000)
+if [ -n "$pid" ]; then
+    echo "Process $pid is using port $port. Killing..."
+    sudo kill $pid
+else
+    echo "No process found using port $port."
+fi
+
 # start the react client
-cd PharmaCareClient
-npm install --force &
+npm_install_function() {
+ cd PharmaCareClient
+ npm install --force
+}
+npm_install_function || true
 npm start &
