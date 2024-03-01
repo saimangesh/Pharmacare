6378fd664239d6196cc36a32124dc0f9fd9fbe78
Sai Mangesh <47791273+saimangesh@users.noreply.github.com>
2024-03-02 02:08:13 +0530

    Update server.sh

--- jenkins/server.sh
+++ jenkins/server.sh
@@ -1,2 +1,11 @@
+# kill the application server
+pid=$(sudo lsof -t -i:9090)
+if [ -n "$pid" ]; then
+    echo "Process $pid is using port $port. Killing..."
+    sudo kill $pid
+else
+    echo "No process found using port $port."
+fi
+
 # start the appliation server
 java -jar PharmaCareServer/target/pharmacare.jar &
