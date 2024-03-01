2804cada6d6bc581316c9df32bec11bc80c03851
Sai Mangesh <47791273+saimangesh@users.noreply.github.com>
2024-03-01 09:25:06 +0530

    Update script.sh

--- jenkins/script.sh
+++ jenkins/script.sh
@@ -2,9 +2,17 @@
 # enter as admin
 sudo su
 
+# kill the existing process
+kill -9 $(lsof -t -i:9090)
+kill -9 $(lsof -t -i:3000)
+
+# replace the ip address
+
+
 # start the appliation server
 java -jar PharmaCareServer/target/pharmacare.jar &
 
 # start the react client
+cd PharmaCareClient
 npm install --force
 npm start &
